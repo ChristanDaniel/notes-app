b3f0b619ac843c174de62d421bfafe9deaa46052
ChristianDaniel <christian_daniel04@hotmail.com>
2021-08-30 21:00:44 -0300

    css footer

--- src/components/Footers/styles.ts
+++ src/components/Footers/styles.ts
@@ -1,5 +1,23 @@
 import styled from 'styled-components'
 
-const Footer = styled.footer``
+const Footer = styled.footer`
+  /* margin-top: 100px; */
+  margin: 100px 35px 0px;
+
+  div {
+    margin: 0 0 8px 80px;
+  }
+
+  p {
+    color: #616480;
+    margin-bottom: 12px;
+    font-size: smaller;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: underline;
+  }
+`
 
 export { Footer }
