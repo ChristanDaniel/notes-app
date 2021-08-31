d3ded0dd875d7b0968ca8885ac63379db3640fec
ChristianDaniel <christian_daniel04@hotmail.com>
2021-08-30 21:00:15 -0300

    footer separado em component

--- src/components/Footers/index.tsx
+++ src/components/Footers/index.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Footer } from './styles'
 
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
-export function Footers() {
+export function Footers(): JSX.Element {
   return (
-    
+    <>
+      <Footer>
+        <div>
+          <a href="https://github.com/ChristanDaniel" target="_blank" rel="noreferrer noopener">
+            <FaGithub /> Github
+          </a>
+          <span> | </span>
+          <a href="https://www.linkedin.com/in/christian-daniel-841921210/" target="_blank" rel="noreferrer noopener">
+            <FaLinkedin /> Linkedin
+          </a>
+        </div>
+        <p>
+          <span>© {new Date().getFullYear()} Notepad-App</span>
+          {' · '}
+          <span>Construido por Christian Daniel .</span>
+        </p>
+      </Footer>
+    </>
   )
 }
--- src/feature/home/HomeContainer.tsx
+++ src/feature/home/HomeContainer.tsx
@@ -8,13 +8,13 @@ import profilePic from '../../../public/noteapp.png'
 // import { format } from 'date-fns'
 // import ptBR from 'date-fns/locale/pt-BR'
 
-import { HomeAside, HomeContainerBg, HomeSection, NoteContainer, NoteContainerDiv, TextareaAutosizeWrapper, SubtitleContainer, Footer } from './styles'
+import { HomeAside, HomeContainerBg, HomeSection, NoteContainer, NoteContainerDiv, TextareaAutosizeWrapper, SubtitleContainer } from './styles'
 
-import { FaGithub, FaLinkedin } from 'react-icons/fa'
 import { RiEdit2Fill, RiDeleteBin7Fill, RiDeleteBinFill } from 'react-icons/ri'
 
 // import toast, { Toaster } from 'react-hot-toast'
 import Button from '../../components/Button'
+import { Footers } from '../../components/Footers'
 
 type Note = {
   id: number
@@ -101,22 +101,7 @@ const HomeContainer: ReactNode = () => {
           <Button type="submit">Enviar Nota</Button>
         </form>
 
-        <Footer>
-          <div>
-            <a href="https://github.com/ChristanDaniel" target="_blank" rel="noreferrer noopener">
-              <FaGithub /> Github
-            </a>
-            <span> | </span>
-            <a href="https://www.linkedin.com/in/christian-daniel-841921210/" target="_blank" rel="noreferrer noopener">
-              <FaLinkedin /> Linkedin
-            </a>
-          </div>
-          <p>
-            <span>© {new Date().getFullYear()} Notepad-App</span>
-            {' · '}
-            <span>Construido por Christian Daniel .</span>
-          </p>
-        </Footer>
+        <Footers />
       </HomeAside>
 
       <HomeSection>
