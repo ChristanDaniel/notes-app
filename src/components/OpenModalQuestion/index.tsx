68d709d76c1ebfdb5ff8b8c233bb9af462c9b308
ChristianDaniel <christian_daniel04@hotmail.com>
2021-12-26 14:28:00 -0300

    transferindo todo Modal para Component OpenModalQuestion

--- src/components/OpenModalQuestion/index.tsx
+++ src/components/OpenModalQuestion/index.tsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React, { FormEvent, useContext } from 'react'
+import Modal from 'react-modal'
+import { QuestionsContainerContext } from '../../NotesContext'
+import Button from '../Button'
+// import {} from './styles'
+
+import Image from 'next/image'
+import closeImg from '../../../public/close.svg'
 
 interface OpenModalQuestionProps {
   isOpen: boolean
@@ -6,5 +13,37 @@ interface OpenModalQuestionProps {
 }
 
 export function OpenModalQuestion({ isOpen, onRequestClose }: OpenModalQuestionProps): JSX.Element {
-  return <></>
+  const { questions, setQuestions, inputEdit, setInputEdit, currentIdQuestion } = useContext(QuestionsContainerContext)
+
+  function handleEditModal(event: FormEvent) {
+    event.preventDefault()
+
+    const NotaEdited = questions.map((question) => {
+      if (question.id === currentIdQuestion?.id) {
+        return { ...question, description: inputEdit }
+      } else {
+        return { ...question }
+      }
+    })
+    setQuestions(NotaEdited)
+
+    onRequestClose()
+  }
+  return (
+    <>
+      <Modal isOpen={isOpen} onRequestClose={onRequestClose} overlayClassName="react-modal-overlay" className="react-modal-content">
+        <button type="button" onClick={onRequestClose} className="react-modal-close">
+          <Image src={closeImg} alt="Fechar Modal" />
+        </button>
+        <div>
+          <h2>Editar Nota</h2>
+          {/* <span>Nota - {questions.indexOf()}</span> */}
+        </div>
+        <form>
+          <textarea aria-label="minimum height" placeholder="Escreva aqui..." onChange={(event) => setInputEdit(event.target.value)} value={inputEdit} />
+          <Button onClick={(event) => handleEditModal(event)}>Editar Nota</Button>
+        </form>
+      </Modal>
+    </>
+  )
 }
