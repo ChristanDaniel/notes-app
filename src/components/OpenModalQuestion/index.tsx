c265089cb73b18d22a3c3315de4bb34a2ff81ec7
ChristianDaniel <christian_daniel04@hotmail.com>
2021-12-26 14:27:19 -0300

    create component OpenModaQuestions

---
+++ src/components/OpenModalQuestion/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+interface OpenModalQuestionProps {
+  isOpen: boolean
+  onRequestClose: () => void
+}
+
+export function OpenModalQuestion({ isOpen, onRequestClose }: OpenModalQuestionProps): JSX.Element {
+  return <></>
+}
